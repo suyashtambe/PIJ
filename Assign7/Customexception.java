7afef145411cea659603b6c1ef4069d087cd033b
suyashtambe <116366583+suyashtambe@users.noreply.github.com>
2024-03-09 17:46:57 +0530

    Changes to be committed: 	deleted:    Assign 5/class.java 	deleted:    Assign 6/ArrayStack.java 	deleted:    Assign 6/Stack.class 	deleted:    Assign 6/Stack.java 	deleted:    Assign 6/UserInput.java 	new file:   Assign5/Assignment 5.pdf 	new file:   Assign5/Circle.java 	new file:   Assign5/Cylinder.java 	new file:   Assign5/Main.java 	new file:   Assign5/Pyramid.java 	new file:   Assign5/Rectangle.java 	new file:   Assign5/Shape.java 	new file:   Assign5/Sphere.java 	new file:   Assign5/Square.java 	new file:   Assign5/Volume.java 	new file:   Assign6/Assignment 6.pdf 	new file:   Assign6/Part1/Assignment 6.pdf 	new file:   Assign6/Part1/Fixed_STK.java 	new file:   Assign6/Part1/Growable_stk.java 	new file:   Assign6/Part1/Interface_STK.java 	new file:   Assign6/Part1/Main.java 	new file:   Assign6/Part2/DecoyDuck.java 	new file:   Assign6/Part2/Duck.java 	new file:   Assign6/Part2/FlyBehaviour.java 	new file:   Assign6/Part2/FlyNoWay.java 	new file:   Assign6/Part2/FlyWithWings.java 	new file:   Assign6/Part2/Main.java 	new file:   Assign6/Part2/Quack.java 	new file:   Assign6/Part2/QuackBehaviour.java 	new file:   Assign6/Part2/RedHeadDuck.java 	new file:   Assign6/Part2/Squeak.java 	new file:   Assign6/Part2/Swim.java 	new file:   Assign6/Part2/SwimBehaviour.java 	new file:   Assign6/Part2/SwimNoWay.java 	new file:   Assign7/Assignment 7.pdf 	new file:   Assign7/Calculator.java 	modified:   Assign7/Customexception.java 	modified:   Assign7/Main.java 	new file:   Assign7/UserInput.java

--- Assign7/Customexception.java
+++ Assign7/Customexception.java
@@ -1,13 +1,28 @@
+// CustomException.java
 package Assign7;
 
-public class Customexception extends Exception {
-   
-        public Customexception(){
-            super("exception has occurred");
+// CustomException class extends Exception to handle custom exceptions
+public class CustomException extends Exception {
+    // Default constructor for CustomException class
+    public CustomException(){
+        super("Division by 0 error"); // Set default message for division by zero error
+    }
+
+    // Parameterized constructor for CustomException class to handle custom error messages
+    public CustomException(String msg){
+        super(msg); // Set custom error message
+    }
+
+    // Nested class NonNumericInputException extends Exception to handle non-numeric input errors
+    public static class NonNumericInputException extends Exception {
+        // Default constructor for NonNumericInputException class
+        public NonNumericInputException() {
+            super("Non-numeric input error"); // Set default message for non-numeric input error
         }
-        public Customexception(String msg){
-            super(msg);
+
+        // Parameterized constructor for NonNumericInputException class to handle custom error messages
+        public NonNumericInputException(String msg) {
+            super(msg); // Set custom error message
         }
     }
-    
-
+}
--- Assign7/Main.java
+++ Assign7/Main.java
@@ -1,24 +1,65 @@
+/*Suyash Tambe
+AIML-B2
+2022-26
+22070126117
+*/
+//Main.java
 package Assign7;
-
-import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) {
+        // Create an instance of UserInput to handle user input
+        UserInput userInput = new UserInput();
+        
         try {
-            int a = 1;
-            System.out.println("Value of a is: " + a);
-            int b = 1;
-            System.out.println("Value of b is: " + b);
-            int c = a / b; // This line will throw ArithmeticException
-            System.out.println("Value of c is: " + c); // This line will not be executed
-            throw new Customexception();
-        } catch (Customexception e) {
-            System.out.println("ArithmeticException caught: " + e);
-        }finally { // tp create the logs
-        	System.out.println("Run the program again");
+            // Prompt the user to choose an operation
+            String choice = userInput.getStringInput("Choose Operation: +, -, /, *, ^, var, stddev, avg");
+
+            // Check if the chosen operation is a basic arithmetic operation or a statistical operation
+            if (choice.equals("+") || choice.equals("-") || choice.equals("*") || choice.equals("/") || choice.equals("^")) {
+                // For basic arithmetic operations, prompt the user to enter two numbers
+                double n1 = userInput.getDoubleInput("Enter First Number:");
+                double n2 = userInput.getDoubleInput("Enter Second Number:");
+                userInput.closeScanner(); // Close the scanner since input is complete
+                
+                // Perform the chosen operation and display the result
+                if (choice.equals("+")) {
+                    System.out.println(Calculator.add(n1, n2));
+                } else if (choice.equals("-")) {
+                    System.out.println(Calculator.subtract(n1, n2));
+                } else if (choice.equals("*")) {
+                    System.out.println(Calculator.multiply(n1, n2));
+                } else if (choice.equals("/")) {
+                    // Handle division by zero exception
+                    try {
+                        System.out.println(Calculator.divide(n1, n2));
+                    } catch (CustomException e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else if (choice.equals("^")) {
+                    System.out.println(Calculator.power(n1, n2));
+                } else {
+                    System.out.println("Invalid Operation");
+                }
+            } else {
+                // For statistical operations, prompt the user to enter an array of numbers
+                double[] arr = userInput.getArrayInput();
+                userInput.closeScanner(); // Close the scanner since input is complete
+                
+                // Perform the chosen statistical operation and display the result
+                if (choice.equals("var")) {
+                    System.out.println(Calculator.variance(arr));
+                } else if (choice.equals("stddev")) {
+                    System.out.println(Calculator.stddev(arr));
+                } else if (choice.equals("avg")) {
+                    System.out.println(Calculator.mean(arr));
+                } else {
+                    System.out.println("Invalid Operation");
+                }
+            }
+        } catch (CustomException.NonNumericInputException e) {
+            // Handle non-numeric input exception
+            System.out.println(e.getMessage());
         }
     }
 }
-
-
